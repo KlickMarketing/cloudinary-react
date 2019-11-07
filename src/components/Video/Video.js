e693ae856944a31600a865cfa6e5810d3cd017ba
Ryan Johnson <ryan@shakainteractive.com>
2019-11-06 21:28:30 -0500

    Testing forked branch

--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import {Cloudinary, Transformation, Util} from 'cloudinary-core';
-import CloudinaryComponent from '../CloudinaryComponent';
+import React from "react";
+import PropTypes from "prop-types";
+import { Cloudinary, Transformation, Util } from "cloudinary-core";
+import CloudinaryComponent from "../CloudinaryComponent";
 
 const DEFAULT_POSTER_OPTIONS = {
-  format: 'jpg',
-  resource_type: 'video'
+  format: "jpg",
+  resource_type: "video"
 };
 
+console.log("KLICK VERSION!");
 /**
  * A component representing a Cloudinary served video
  */
@@ -18,9 +19,14 @@ class Video extends CloudinaryComponent {
   }
 
   render() {
-    let {publicId, poster, sourceTypes, fallback, sourceTransformation: sourceTransformations, ...options} = Object.assign({},
-      this.getContext(),
-      this.props);
+    let {
+      publicId,
+      poster,
+      sourceTypes,
+      fallback,
+      sourceTransformation: sourceTransformations,
+      ...options
+    } = Object.assign({}, this.getContext(), this.props);
     sourceTransformations = sourceTransformations || {};
     sourceTypes = sourceTypes || Cloudinary.DEFAULT_VIDEO_PARAMS.source_types;
     options = CloudinaryComponent.normalizeOptions(options, {});
@@ -29,8 +35,14 @@ class Video extends CloudinaryComponent {
     let tagAttributes = Transformation.new(options).toHtmlAttributes();
     let childTransformations = this.getTransformation(options);
     if (Util.isPlainObject(poster)) {
-      let defaults = poster.publicId !== undefined && poster.publicId !== null ? Cloudinary.DEFAULT_IMAGE_PARAMS : DEFAULT_POSTER_OPTIONS;
-      poster = cld.url(poster.publicId || publicId, Util.defaults({}, Util.withSnakeCaseKeys(poster), defaults));
+      let defaults =
+        poster.publicId !== undefined && poster.publicId !== null
+          ? Cloudinary.DEFAULT_IMAGE_PARAMS
+          : DEFAULT_POSTER_OPTIONS;
+      poster = cld.url(
+        poster.publicId || publicId,
+        Util.defaults({}, Util.withSnakeCaseKeys(poster), defaults)
+      );
     }
     if (!Util.isEmpty(poster)) {
       tagAttributes.poster = poster;
@@ -41,15 +53,26 @@ class Video extends CloudinaryComponent {
 
     if (Util.isArray(sourceTypes)) {
       sources = sourceTypes.map(srcType => {
-          let sourceTransformation = sourceTransformations[srcType] || {};
-          let src = cld.url(publicId, Util.defaults({}, sourceTransformation, childTransformations, {resource_type: 'video', format: srcType}));
-          let mimeType = 'video/' + (srcType === 'ogv' ? 'ogg' : srcType);
-          return <source key={mimeType} src={src} type={mimeType}/>;
-        }
-      );
+        let sourceTransformation = sourceTransformations[srcType] || {};
+        let src = cld.url(
+          publicId,
+          Util.defaults({}, sourceTransformation, childTransformations, {
+            resource_type: "video",
+            format: srcType
+          })
+        );
+        let mimeType = "video/" + (srcType === "ogv" ? "ogg" : srcType);
+        return <source key={mimeType} src={src} type={mimeType} />;
+      });
     } else {
       let sourceTransformation = sourceTransformations[sourceTypes] || {};
-      tagAttributes.src = cld.url(publicId, Util.defaults({}, sourceTransformation, childTransformations, {resource_type: 'video', format: sourceTypes}));
+      tagAttributes.src = cld.url(
+        publicId,
+        Util.defaults({}, sourceTransformation, childTransformations, {
+          resource_type: "video",
+          format: sourceTypes
+        })
+      );
     }
 
     return (
@@ -61,7 +84,7 @@ class Video extends CloudinaryComponent {
     );
   }
 }
-Video.propTypes = {publicId: PropTypes.string};
+Video.propTypes = { publicId: PropTypes.string };
 Video.defaultProps = {};
 Video.contextTypes = CloudinaryComponent.contextTypes;
 
